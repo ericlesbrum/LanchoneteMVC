56237ed71520dbea4bdebed08334855d922b76f0
Ericles Brum <ericlespbrum@gmail.com>
2024-07-09 16:55:27 -0300

    Implementando a view model `LancheListViewModel`

---
+++ Models/CarrinhoCompraItem.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchoneteMVC.Models
+{
+    [Table("CarrinhoCompraItens")]
+    public class CarrinhoCompraItem
+    {
+        public int Id { get; set; }
+        public Lanche Lanche { get; set; }
+        public int Quantidade { get; set; }
+        [StringLength(200)]
+        public string CarrinhoCompraId { get; set; }
+    }
+}
