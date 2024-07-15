5f7ada2f16c9a6dbec2370dd2942b70e0ac72485
Ericles Brum <ericlespbrum@gmail.com>
2024-07-15 16:10:08 -0300

    Ajustando o `LancheController` para exibir lanches por `categoria`

--- Controllers/LancheController.cs
+++ Controllers/LancheController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using LanchoneteMVC.Models;
 using LanchoneteMVC.Repositories.Interfaces;
 using LanchoneteMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace LanchoneteMVC.Controllers
 {
@@ -19,14 +21,27 @@ namespace LanchoneteMVC.Controllers
             _lancheRepository = lancheRepository;
         }
 
-        public IActionResult List()
+        public IActionResult List(string categoria)
         {
-            // var lanches= _lancheRepository.Lanches;
-            // return View(lanches);
-            var lanchesListViewModel= new LancheListViewModel();
-            lanchesListViewModel.Lanches =_lancheRepository.Lanches;
-            lanchesListViewModel.CategoriaAtual="Categoria Atual";
-            return View(lanchesListViewModel);
+            IEnumerable<Lanche> lanches;
+
+            if (string.IsNullOrEmpty(categoria))
+            {
+                lanches = _lancheRepository.Lanches.OrderBy(lanche => lanche.Id);
+                return View(LancheListViewModel.GetLancheListViewModel(lanches, "Todos os Lanches"));
+            }
+            switch (categoria.ToLower())
+            {
+                case "normal":
+                    lanches = _lancheRepository.Lanches.Where(lanche => lanche.Categoria.CategoriaNome.Equals("Normal")).OrderBy(lanche => lanche.Nome);
+                    return View(LancheListViewModel.GetLancheListViewModel(lanches, "Normal"));
+                case "natural":
+                    lanches = _lancheRepository.Lanches.Where(lanche => lanche.Categoria.CategoriaNome.Equals("Natural")).OrderBy(lanche => lanche.Nome);
+                    return View(LancheListViewModel.GetLancheListViewModel(lanches, "Natural"));
+                default:
+                    lanches = _lancheRepository.Lanches.OrderBy(lanche => lanche.Id);
+                    return View(LancheListViewModel.GetLancheListViewModel(lanches, "Todos os Lanches"));
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- ViewModels/LancheListViewModel.cs
+++ ViewModels/LancheListViewModel.cs
@@ -8,7 +8,18 @@ namespace LanchoneteMVC.ViewModels
 {
     public class LancheListViewModel
     {
+
         public IEnumerable<Lanche> Lanches { get; set; }
         public string CategoriaAtual { get; set; }
+
+        public static LancheListViewModel GetLancheListViewModel(IEnumerable<Lanche> lanche, string categoria)
+        {
+            return new LancheListViewModel
+            {
+                Lanches = lanche,
+                CategoriaAtual = categoria
+            };
+
+        }
     }
 }
